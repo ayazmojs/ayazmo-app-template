5df5ebca5d96ba4a84131bb6cbd4d2b4eb141557
Tsvetan Nikolov <tsvetan.nikolov@coliquio.de>
2024-01-04 19:27:39 +0200

    config and .env

---
+++ ayazmo.config.js
@@ -0,0 +1,30 @@
+import { config } from 'dotenv';
+import fs from 'node:fs';
+import path from 'node:path';
+
+// Determine the environment and construct the filename
+const environment = process.env.NODE_ENV || 'development';
+const envPath = path.resolve(process.cwd(), `.env.${environment}`);
+
+// Check if the environment-specific file exists
+if (fs.existsSync(envPath)) {
+  config({ path: envPath });
+} else {
+  config(); // Load the default .env file
+}
+
+export default {
+  database: {
+    // Default database configurations (can be overridden with environment variables)
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USERNAME || 'ayazmo-user',
+    password: process.env.DB_PASSWORD || 'ayazmo-password',
+    dbName: process.env.DB_DATABASE || 'ayazmo-dev',
+    debug: false,
+    // Add any other database-specific configurations
+  },
+  plugins: [
+    // Activate plugins here
+  ]
+}
