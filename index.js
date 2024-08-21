40d23c99039b25a35f4a7bbea16951b5b04aa3d0
Tsvetan Nikolov <tsvetan.nikolov@coliquio.de>
2024-08-21 18:54:47 +0300

    allow server options

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import { Server } from '@ayazmo/core';
+import pino from 'pino'
+
+const logger = pino({
+  level: 'debug',
+  transport: {
+    target: 'pino-pretty',
+  },
+})
 
 ;(async() => {
   async function start() {
-    const app = new Server()
+    const app = new Server({
+      logger,
+    })
     await app.start()
   }
 
